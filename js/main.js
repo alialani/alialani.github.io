8e2eda65021d773346c96d2240dbad565fbbd0ab
alialani0719 <alialani0719@gmail.com>
2015-11-05 08:09:45 -0500

    Some work committed

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+
+	$('.view-details').on('click', function(event){
+		var targetElement = event.target;
+	   // Within that parent, all the elements that have the class `details`.
+		var container = targetElement.parentElement.parentElement;
+		
+		$(container).find('.details').each(function(index, el){
+		    // Toggle visibility of all the elements within that parent with the class `details`.
+			// Change the text of the "view details" button to read "hide details" so the user
+  		    // understands they can hide the text again.
+			if($(el).is(':visible')) {
+			   $(el).fadeOut();
+			   targetElement.innerText = 'show details';
+			} else {
+			   $(el).fadeIn();
+			   targetElement.innerText = 'hide details';
+			}
+		});
+	});
+
+	
+});
